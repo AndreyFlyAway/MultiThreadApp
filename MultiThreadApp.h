8d89c75ee9646aeee614cc5c16558528c5faee0a
AndreyFlyAway <ggg235235@gmail.com>
2020-04-14 15:48:55 +0500

    Добавлна некоторая оптимизация кода.

--- MultiThreadApp.h
+++ MultiThreadApp.h
@@ -20,16 +20,21 @@
 
 /* структура, для хранения данных о задаче*/
 struct task_t {
-    pthread_t pt_id;            // для использоованя с API pthread
-    uint task_id ;              // назначется вручную через глобальный счетсчик g_task_coun
-    int delay_sec;              // задержка запуска задачи
-    time_t time_started;      // время добавления задачи чтобы, отсчитывать и выводить время, через которое очнеться задача
+    pthread_t pt_id;           // для использоованя с API pthread
+    uint task_id ;             // назначется вручную через глобальный счетсчик g_task_coun
+    int delay_sec;             // задержка запуска задачи
+    time_t time_started;       // время добавления задачи чтобы, отсчитывать и выводить время, через которое очнеться задача
     int progress;              // прогресс задачи
     int status;                // код статуса 1 - запущена, 2 - в ожидании
     // void task_func();
 };
 
-
+/* common functions */
+int get_task_by_id(uint task_id, task_t &trgt);
+void set_task_status(task_t *tsk, int status);
+int add_percentage(task_t *tsk, int percent);
+bool is_number(const std::string& s);
+/*  */
 int get_task_info(std::vector<std::string> data);
 void get_task_info(task_t *tsk);
 int task_mannger(std::string cmd);
